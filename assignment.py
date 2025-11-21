a20ba5dbcc7fa41899ada79eeb6a5e2919f64a1a
Monique Vega <Moniquevega090@gmail.com>
2025-11-21 09:14:56 -0800

    This is my assignment

--- assignment.py
+++ assignment.py
@@ -3,6 +3,6 @@ cars = ["bmw", "audi", "toyota", "subaru"]
 cars.sort()
 print(cars)
 vehicles = ["bmw", "audi", "toyota", "subaru"]
-vehicles.sort(reverse=True)
-print(vehicles)
+vehicle.sort(reverse=True)
+print(vehicle)
 
