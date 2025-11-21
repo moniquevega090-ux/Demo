12d8a8bc04bb7dfcaf0c4304077e4033c2e5890b
Monique Vega <Moniquevega090@gmail.com>
2025-11-21 09:13:34 -0800

    This is my assignment

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
 print("Hello Python interpreter!")
+cars = ["bmw", "audi", "toyota", "subaru"]
+cars.sort()
+print(cars)
+vehicles = ["bmw", "audi", "toyota", "subaru"]
+vehicles.sort(reverse=True)
+print(vehicles)
+
